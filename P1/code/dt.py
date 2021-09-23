80e85121dd2413b63d94e6208a5a4371583ba623
Julien-Gustin <juliengustin02@gmail.com>
2021-09-23 23:14:45 +0200

    Code refractor + doc

--- P1/code/dt.py
+++ P1/code/dt.py
@@ -21,13 +21,12 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 # Put your funtions here
 # ...
 import os
-from graphviz import Source
-from sklearn.tree import export_graphviz
 
 PROJECT_ROOT_DIR = ".."
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
 
-def split_train_test(train_size=1000, samples=3000):
+
+def split_train_test(train_size: int = 1000, samples: int = 3000) -> tuple:
     """Return the train and test set
 
     Args:
@@ -42,44 +41,84 @@ def split_train_test(train_size=1000, samples=3000):
     """
     X_full, y_full = make_unbalanced_dataset(3000, random_state=42)
 
-    X_train, X_test = X_full[:1000], X_full[1000:] 
+    X_train, X_test = X_full[:1000], X_full[1000:]
     y_train, y_test = y_full[:1000], y_full[1000:]
     return X_train, X_test, y_train, y_test
 
-def create_models(min_samples_splits, seed, X, y):
+
+def create_models(
+    min_samples_splits: list, seed: int, X: np.array, y: np.array
+) -> list:
+    """Create, fit and returns size(min_samples_splits) DecisionTreeClassifier models
+
+    Args:
+        min_samples_splits (list): array containing the min_samples_split parameter for each mode
+        seed (int): random seed
+        X (np.array): The inputs samples
+        y (np.array): The output samples
+
+    Returns:
+        tree_clfs (list): List of tree classifer model
+    """
+
     tree_clfs = []
     for min_samples_split in min_samples_splits:
-        tree_clf = DecisionTreeClassifier(min_samples_split=min_samples_split, random_state=42)
+        tree_clf = DecisionTreeClassifier(
+            min_samples_split=min_samples_split, random_state=42
+        )
         tree_clf.fit(X_train, y_train)
         tree_clfs.append(tree_clf)
 
     return tree_clfs
 
-def make_plots(tree_model, X, y):
+
+def make_plots(
+    tree_model: DecisionTreeClassifier,
+    X: np.array,
+    y: np.array,
+    plot_tree: bool = False,
+):
+    """Create plot in the "../images" directory
+
+    Args:
+        tree_model (DecisionTreeClassifier): the model used for the plots
+        X (np.array): The inputs samples
+        y (np.array): The output samples
+        plot_tree (bool): Plot the decision tree WARNING (need to install "graphviz")
+    """
+
     samples_split = tree_clf.get_params()["min_samples_split"]
-    plot_boundary(os.path.join(IMAGES_PATH, "dt_{}_samples_split".format(samples_split)), tree_model, X, y)
-
-    export_graphviz(
-        tree_clf,
-        out_file="out.dot",
-        rounded=True,
-        filled=True
+    plot_boundary(
+        os.path.join(IMAGES_PATH, "dt_{}_samples_split".format(samples_split)),
+        tree_model,
+        X,
+        y,
     )
+    
+    try:
+        from graphviz import Source
+        from sklearn.tree import export_graphviz
+        
+        export_graphviz(tree_clf, out_file="out.dot", rounded=True, filled=True)
 
-    Source.from_file("out.dot")
-    os.system("dot -Tpng out.dot -o" + os.path.join(IMAGES_PATH, "tree_{}_samples_split.png".format(samples_split)))
-    os.system("rm out.dot")
-
+        Source.from_file("out.dot")
+        os.system(
+            "dot -Tpng out.dot -o"
+            + os.path.join(IMAGES_PATH, "tree_{}_samples_split.png".format(samples_split))
+        )
+        os.system("rm out.dot")
+    except:
+        print("Please install \"graphviz\" or set `plot_tree` to False")
 
 if __name__ == "__main__":
     # Put your code here
 
     X_train, X_test, y_train, y_test = split_train_test()
-    min_samples_splits = [2, 8, 32, 64, 128, 500];
+    min_samples_splits = [2, 8, 32, 64, 128, 500]
 
     tree_clfs = create_models(min_samples_splits, 42, X_train, y_train)
 
     for tree_clf in tree_clfs:
-        make_plots(tree_clf, X_test, y_test)
+        make_plots(tree_clf, X_test, y_test, True)
 
     pass
