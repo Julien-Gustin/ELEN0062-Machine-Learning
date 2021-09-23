fbff3a2fb0cfac72b67af964c79f1279806611fa
Julien-Gustin <juliengustin02@gmail.com>
2021-09-23 12:09:45 +0200

    Code refractor + using test set for plot instead of training set

--- P1/code/dt.py
+++ P1/code/dt.py
@@ -8,6 +8,7 @@ Project 1 - Classification algorithms
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import tree
 
 from plot import plot_boundary
 from data import make_balanced_dataset, make_unbalanced_dataset
@@ -45,27 +46,40 @@ def split_train_test(train_size=1000, samples=3000):
     y_train, y_test = y_full[:1000], y_full[1000:]
     return X_train, X_test, y_train, y_test
 
+def create_models(min_samples_splits, seed, X, y):
+    tree_clfs = []
+    for min_samples_split in min_samples_splits:
+        tree_clf = DecisionTreeClassifier(min_samples_split=min_samples_split, random_state=42)
+        tree_clf.fit(X_train, y_train)
+        tree_clfs.append(tree_clf)
+
+    return tree_clfs
+
+def make_plots(tree_model, X, y):
+    samples_split = tree_clf.get_params()["min_samples_split"]
+    plot_boundary(os.path.join(IMAGES_PATH, "dt_{}_samples_split".format(samples_split)), tree_model, X, y)
+
+    export_graphviz(
+        tree_clf,
+        out_file="out.dot",
+        rounded=True,
+        filled=True
+    )
+
+    Source.from_file("out.dot")
+    os.system("dot -Tpng out.dot -o" + os.path.join(IMAGES_PATH, "tree_{}_samples_split.png".format(samples_split)))
+    os.system("rm out.dot")
+
 
 if __name__ == "__main__":
     # Put your code here
 
     X_train, X_test, y_train, y_test = split_train_test()
+    min_samples_splits = [2, 8, 32, 64, 128, 500];
 
-    for samples_split in [2, 8, 32, 64, 128, 500]:
-        tree_clf = DecisionTreeClassifier(min_samples_split=samples_split, random_state=42)
-        tree_clf.fit(X_train, y_train)
-        plot_boundary(os.path.join(IMAGES_PATH, "dt_{}_samples_split".format(samples_split)), tree_clf, X_train, y_train)
+    tree_clfs = create_models(min_samples_splits, 42, X_train, y_train)
 
+    for tree_clf in tree_clfs:
+        make_plots(tree_clf, X_test, y_test)
 
-        export_graphviz(
-                tree_clf,
-                out_file="out.dot",
-                rounded=True,
-                filled=True
-            )
-
-        Source.from_file("out.dot")
-        os.system("dot -Tpng out.dot -o" + os.path.join(IMAGES_PATH, "tree_{}_samples_split.png".format(samples_split)))
-        
-    os.system("rm out.dot")
     pass
