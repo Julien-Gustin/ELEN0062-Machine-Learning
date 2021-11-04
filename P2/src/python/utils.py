9ace455915b26c6925c2feff9d1787ecd3caacad
Houyon <houyonjoachim@gmail.com>
2021-11-04 14:34:27 +0100

    Q2e

--- P2/src/python/utils.py
+++ P2/src/python/utils.py
@@ -1,15 +1,16 @@
 from scipy.stats import norm, uniform
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
 
-def f(x, seed=42):
-    e = norm.rvs(loc=0, scale=0.5**2, size=len(x), random_state=seed)
+def f(x, scale=0.5, seed=42):
+    e = norm.rvs(loc=0, scale=scale, size=len(x), random_state=seed)
     return np.sin(2*x) + x*np.cos(x - 1) + e
 
-def make_data(N_samples, seed=42):
+def make_data(N_samples, scale=0.5, seed=42):
     X = uniform.rvs(loc=-10, scale=20, size=N_samples, random_state=seed)
     X = np.round(X, decimals=1)
-    Y = f(X, seed=seed)
+    Y = f(X, scale=scale, seed=seed)
     return X, Y
 
 def plot_data(X, Y):
@@ -17,6 +18,7 @@ def plot_data(X, Y):
     plt.xlabel("x")
     plt.ylabel("y")
     plt.scatter(X, Y, alpha=0.5, s=5)
+
 
 
 
