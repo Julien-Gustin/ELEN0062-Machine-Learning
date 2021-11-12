34c2c05cd0d2a09381a45dbda9ee4223cf6b5d72
Julien-Gustin <juliengustin02@gmail.com>
2021-11-12 20:40:30 +0100

    update

--- P2/src/python/protocol.py
+++ P2/src/python/protocol.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.base import clone
+import utils
 
 class Protocol:
     def __init__(self, X, y, n_LS, models) -> None:
@@ -10,14 +11,6 @@ class Protocol:
         self.variance = None
         self.bias_2 = None
         self.models_kind = models
-
-    def get_unique(self):
-        """Get an array containing unique xi in self.X
-
-        Returns:
-            [np.array]: unique xi
-        """
-        return np.unique(self.X)
 
     def split_data_set(self):
         """Split data set in 'n_LS' dataset
@@ -30,41 +23,41 @@ class Protocol:
 
         return X_split, y_split
 
-    def get_noise(self): # step (iii)
-        """ Get an approximation of the noise for each `unique` xi
+    def get_noise(self, x0, scale=0.5): # step (iii)
+        """ Get an approximation of the noise given a (sorted) array of x0
 
         Returns:
-            [np.array]: noise associated to each `unique` xi
+            [np.array]: noise associated to each x0
         """
         if self.noise is not None:
             return self.noise
 
-        X_unique = self.get_unique()
-        noise = np.zeros(len(X_unique))
-        for i, xi in enumerate(X_unique):
-            indexes = self.X == xi
-            y_xi = self.y[indexes]
-            noise[i] = np.var(y_xi)
+        y0 = []
+        for i in range(1000):
+            y0.append(utils.f(x0, scale=scale, seed=i))
 
-        self.noise = noise
-        return noise
+        y0 = np.array(y0)
+        self.noise = np.var(y0, axis=0, ddof=1)
 
-    def get_mean(self): # step (ii)
-        """ Get the mean of y given `unique` xi
+        return self.noise
+
+    def get_mean(self, x0): # step (ii)
+        """ Get the mean of y given an array of (sorted) x0
 
         Returns:
             [np.array]: mean of y for each xi
         """
-        X_unique = self.get_unique()
-        y = np.zeros(len(X_unique))
-        for i, xi in enumerate(X_unique):
-            indexes = self.X == xi
-            y[i] = self.y[indexes].mean()
 
-        return y
+        y0 = []
+        for i in range(1000):
+            y0.append(utils.f(x0, seed=i))
 
-    def get_variance_bias(self):
-        """ Get the variance and squared bias of each `unique` xi for all `k` model in `models`
+        y0 = np.array(y0)
+
+        return np.mean(y0, axis=0)
+
+    def get_variance_bias(self, x0):
+        """ Get the variance and squared bias of each x0 for all `k` model in `models`
 
         Returns:
             [tuple]: [[variance_m0, ... variance_mk], [bias^2_m0, ..., [bias^2_mk]]
@@ -76,8 +69,8 @@ class Protocol:
         self.bias_2 = []
 
         X_split, y_split = self.split_data_set() # step (iv)
-        X_unique = self.get_unique()
-        y_mean = self.get_mean()
+
+        y0_mean = self.get_mean(x0)
 
         for model in self.models_kind:
             predictions = []
@@ -88,24 +81,24 @@ class Protocol:
                 sub_y = sub_y.reshape(-1, 1)
                 model_i.fit(sub_x, sub_y) # step (v)
 
-                y_pred = model_i.predict(X_unique.reshape(-1, 1)) # step (vi)
+                y_pred = model_i.predict(x0.reshape(-1, 1)) # step (vi)
                 predictions.append(y_pred)
 
-            self.variance.append(np.var(predictions, axis=0).flatten())
-            self.bias_2.append((np.mean(predictions, axis=0).flatten() - y_mean)**2)
+            self.variance.append(np.var(predictions, axis=0, ddof=1).flatten())
+            self.bias_2.append((np.mean(predictions, axis=0).flatten() - y0_mean)**2)
 
         return self.variance, self.bias_2
 
-    def get_expected_error(self):
+    def get_expected_error(self, x0):
         """ Get the expected error for each `unique` xi for all `k` model in `models`
 
         Returns:
             [np.array]: [expected_error_m0, ... expected_error_mk]
         """
         if self.variance is None or self.bias_2 is None:
-            self.get_variance_bias()
+            self.get_variance_bias(x0)
 
         if self.noise is None:
-            self.get_noise()
+            self.get_noise(x0)
 
         return self.noise + self.variance + self.bias_2
--- P2/src/python/utils.py
+++ P2/src/python/utils.py
@@ -19,12 +19,11 @@ def make_data(N_samples, scale=0.5, seed=42):
         X, Y
     """
     X = uniform.rvs(loc=-10, scale=20, size=N_samples, random_state=seed)
-    X = np.round(X, decimals=1)
     Y = f(X, scale=scale, seed=seed)
     return X, Y
 
 def plot_data(X, Y):
-    plt.figure(figsize=(12, 8), dpi=80)
+    plt.figure(figsize=(20, 12), dpi=80)
     plt.xlabel("x")
     plt.ylabel("y")
     plt.scatter(X, Y, alpha=0.5, s=5)
