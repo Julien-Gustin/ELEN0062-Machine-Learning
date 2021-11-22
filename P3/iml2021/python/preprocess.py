d6c1cb2f251210b58b6cd6c5136b51deb50104ab
Julien-Gustin <juliengustin02@gmail.com>
2021-11-22 09:10:16 +0100

    lol

---
+++ P3/iml2021/python/preprocess.py
@@ -0,0 +1,76 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import numpy as np
+from sktime.datatypes._panel._convert import (
+    from_3d_numpy_to_nested,
+)
+
+class CleanNanByMedian(BaseEstimator, TransformerMixin):
+    """Clean the data by replacing Nan (-999999.99) by the median of the column
+    """
+    def __init__(self):
+        self.median = 0
+        pass
+
+    def fit(self, X, y=None):
+        X_df = pd.DataFrame(X)
+        self.median = X_df.median()
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+        X_df = pd.DataFrame(X_)
+        X_clean = np.array(X_df.replace(-999999.99, np.nan).fillna(self.median))
+        return X_clean
+
+class TimeSerieMaker(BaseEstimator, TransformerMixin):
+    """Transform data into time series (in order to be used by sktime)
+
+    Returns:
+        Panda time series
+    """
+    def __init__(self):
+        self.std_list = []
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+
+        time_series = []
+
+        for _, xi in enumerate(X_):
+            time_series.append(np.split(xi, 31))
+
+        time_series = np.array(time_series) # [sample][sensor]
+        nested_time_series = from_3d_numpy_to_nested(time_series)
+        return nested_time_series
+
+class SimpleSlidingWindowFeaturesExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, window_size, time_serie_length=512):
+        if (time_serie_length % window_size) != 0:
+            raise Exception("window_size should be a diviser of time_serie_length")
+        self.window_size = window_size 
+        self.time_serie_length = time_serie_length
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+
+        _, columns = X_.shape
+
+        X_output = np.empty((len(X_)))
+        for window_nb in range(columns//self.window_size):
+            stds = X_[:,self.window_size*window_nb:(1+window_nb)*self.window_size].std(axis=1)
+            means = X_[:,self.window_size*window_nb:(1+window_nb)*self.window_size].mean(axis=1)
+            slopes = (X_[:,window_nb] - X_[:,(1+window_nb)*self.window_size -1])/self.window_size
+
+            X_output = np.c_[(X_output, stds, means, slopes)]
+
+        X_output = X_output.T[1:].T # remove `empty` elements
+
+        return X_output
---
+++ P3/iml2021/python/utils.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Antonio Sutera & Yann Claes
+
+import os
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def load_data(data_path):
+
+    FEATURES = range(2, 33)
+    N_TIME_SERIES = 3500
+
+    # Create the training and testing samples
+    LS_path = os.path.join(data_path, 'LS')
+    TS_path = os.path.join(data_path, 'TS')
+    X_train, X_test = [np.zeros((N_TIME_SERIES, (len(FEATURES) * 512))) for i in range(2)]
+
+    for f in FEATURES:
+        data = np.loadtxt(os.path.join(LS_path, 'LS_sensor_{}.txt'.format(f)))
+        X_train[:, (f-2)*512:(f-2+1)*512] = data
+        data = np.loadtxt(os.path.join(TS_path, 'TS_sensor_{}.txt'.format(f)))
+        X_test[:, (f-2)*512:(f-2+1)*512] = data
+    
+    y_train = np.loadtxt(os.path.join(LS_path, 'activity_Id.txt'))
+
+    print('X_train size: {}.'.format(X_train.shape))
+    print('y_train size: {}.'.format(y_train.shape))
+    print('X_test size: {}.'.format(X_test.shape))
+
+    return X_train, y_train, X_test
+
+
+def write_submission(y, where, submission_name='toy_submission.csv'):
+
+    os.makedirs(where, exist_ok=True)
+
+    SUBMISSION_PATH = os.path.join(where, submission_name)
+    if os.path.exists(SUBMISSION_PATH):
+        os.remove(SUBMISSION_PATH)
+
+    y = y.astype(int)
+    outputs = np.unique(y)
+
+    # Verify conditions on the predictions
+    if np.max(outputs) > 14:
+        raise ValueError('Class {} does not exist.'.format(np.max(outputs)))
+    if np.min(outputs) < 1:
+        raise ValueError('Class {} does not exist.'.format(np.min(outputs)))
+    
+    # Write submission file
+    with open(SUBMISSION_PATH, 'a') as file:
+        n_samples = len(y)
+        if n_samples != 3500:
+            raise ValueError('Check the number of predicted values.')
+
+        file.write('Id,Prediction\n')
+
+        for n, i in enumerate(y):
+            file.write('{},{}\n'.format(n+1, int(i)))
+
+    print('Submission {} saved in {}.'.format(submission_name, SUBMISSION_PATH))
+
+if __name__ == '__main__':
+
+    # Directory containing the data folders
+    DATA_PATH = 'data'
+    X_train, y_train, X_test = load_data(DATA_PATH)
+
+    clf = KNeighborsClassifier(n_neighbors=1)
+    clf.fit(X_train, y_train)
+
+    y_test = clf.predict(X_test)
+
+    write_submission(y_test, 'submissions')
